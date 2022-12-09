3afb34bf81d594d5230b9c0c06cee97503ee01f0
ForynX <kamstar87@gmail.com>
2022-12-09 11:23:25 +0100

    Error subject emitted: on request error in error.interceptor.ts

--- src/app/interceptors/error.interceptor.ts
+++ src/app/interceptors/error.interceptor.ts
@@ -6,18 +6,20 @@ import {
   HttpInterceptor,
   HttpErrorResponse
 } from '@angular/common/http';
-import { catchError, Observable, retry, throwError} from 'rxjs';
+import { catchError, Observable, retry, Subject, throwError} from 'rxjs';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
   constructor() {}
 
+  onError: Subject<string> = new Subject<string>();
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       retry(1),
       catchError((error : HttpErrorResponse) => {
-        console.log("test");
+        this.onError.next(error.message);
         return throwError(error);
       })
     );
