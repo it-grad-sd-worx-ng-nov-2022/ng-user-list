8f29d36d8a261bb2a66d66b3e27c79fb01e6eeda
careem <macs270500@gmail.com>
2022-12-09 13:54:38 +0400

    Models created

---
+++ src/app/models/user.ts
@@ -0,0 +1,23 @@
+import { Login } from "./login"
+import { Name } from "./name"
+import { Registered } from "./registered"
+import { Dob } from "./date-of-birth"
+import { Id } from "./id"
+import { Picture } from "./picture"
+
+export interface User {
+
+  gender: string;
+  name: Name;
+  location: Location;
+  email: string;
+  login: Login;
+  dob: Dob;
+  registered: Registered;
+  phone: string;
+  cell: string;
+  id: Id;
+  picture: Picture;
+  nat: string;
+
+}
