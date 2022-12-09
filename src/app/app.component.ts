8c631fbd2001e61aa2288ac028c5aa2eb00194cb
Patryk Musial <patrykmusial@gmail.com>
2022-12-09 14:00:57 +0100

    cleaning up error alert test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,14 +30,13 @@ export class AppComponent implements OnInit {
           imageUrl: 'https://thumbs.dreamstime.com/b/stressed-virtual-reality-developer-out-programmer-working-game-app-79348096.jpg',
           imageWidth: 400,
           imageHeight: 266,
-          title: 'Gentleman, we have a problem',
+          title: 'Gentleman, we have a problem with the http request',
           timer: 5000,
           backdrop: false,
           showConfirmButton:false
           })
     }) ;
-    console.error('BRRRT');
-    this.errorService.onError.next('Test error');
+    // this.errorService.onError.next('Test error');
   } 
   
 }
