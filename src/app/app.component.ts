7c27b3b50ed9b5c17838f0e1cd757fe3e6f04936
Patryk Musial <patrykmusial@gmail.com>
2022-12-09 11:44:25 +0100

    Adding Error alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import SWAL from 'sweetalert2'
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Ng User List';
+export class AppComponent implements OnInit {
+ 
+  constructor(
+   public errorInterceptor: ErrorInterceptor
+  ) {
+    
+  }
+  
+  ngOnInit(): void {
+    
+    this.errorInterceptor.onError.subscribe((message) => {
+      console.log('errrr');
+       SWAL.fire({
+          position: 'top-end',
+          icon: 'error',
+          title: 'Gentleman, we have a problem',
+          timer: 5000,
+          backdrop: false
+          })
+    }) ;
+    this.errorInterceptor.onError.next('eroorrrr');
+  } 
+  
 }
+
+
