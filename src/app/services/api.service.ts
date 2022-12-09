557b5e2587aedda300d659fc4199ceb2c2bd660e
Robert <robertkoszowski91@gmail.com>
2022-12-09 14:53:10 +0100

    updated api

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,47 +1,33 @@
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-
 
 @Injectable({
+
   providedIn: 'root'
+
 })
+
 export class ApiService {
 
   baseUrl: string = "https://randomuser.me/api/?results=20&seed=";
-
-  list: {[k:string]:string | any} = {};
-
+  
   constructor(public http: HttpClient) {
+
   }
 
   endpoints: { [endpoint: string]: string | any } = {
-    userList: `${this.baseUrl}`, 
+
+    userList: `${this.baseUrl}`,
     students: `${this.baseUrl}students`,
     alumni: `${this.baseUrl}alumni`,
     teachers: `${this.baseUrl}teachers`
-    // userDetails:(id: string)=> {
-    //   return new Observable((subscriber) => {
-    //     subscriber.next(this.getUserById(id));
-    //     subscriber.complete();
-    //   })
-    // },
-  }
-
-  getUserById(id:string){
-    console.log('Pseudo ID:',id);
-    return this.list;
   }
 
   request(url: endpointType, method: string, payload?: object, urlParams?: any) {
-    // if(urlParams) return  this.endpoints[url](urlParams);
-    // return this.http.request(method, this.endpoints[url]).pipe(tap((result)=>{console.log(result);this.list=result}))
-    // const finalUrl = !urlParams ? this.endpoints[url] : this.endpoints[url](urlParams);
-    // return !payload ? this.http.request(method, finalUrl).pipe(tap((result)=>{this.list=result})) : finalUrl;
+    const finalUrl = !urlParams ? this.endpoints[url] : this.endpoints[url](urlParams);
+    return !payload ? this.http.request(method, finalUrl) : this.http.request(method, finalUrl, {body : payload});
   }
+
 }
 
-
-
-export type endpointType = 'userList' | 'students' | 'teachers' | 'alumni'; 
+export type endpointType = 'userList' | 'students' | 'teachers' | 'alumni';
