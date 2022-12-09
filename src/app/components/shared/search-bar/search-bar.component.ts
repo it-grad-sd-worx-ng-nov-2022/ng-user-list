fed2d54ec78c720dc6c96f1a9fcc3366462fb581
Yogeshm1699 <yogeshmohall@yahoo.com>
2022-12-09 13:59:03 +0400

    Dynamic Search bar html

--- src/app/components/shared/search-bar/search-bar.component.ts
+++ src/app/components/shared/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  @Input() arrayTitle?:string;
+  searchText:string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+    this.clearSearchText();
+  }
+
+  searchArrary() {
+
+  }
+
+  clearSearchText() {
+
+    this.searchText = '';
   }
 
 }
