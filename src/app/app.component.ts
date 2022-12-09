51be7822d68dfc673ed7eb939026eef5e76d4171
Patryk Musial <patrykmusial@gmail.com>
2022-12-09 14:31:31 +0100

    Further work on error handling + error alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import SWAL from 'sweetalert2'
 import { ErrorInterceptor } from './interceptors/error.interceptor';
 import { ApiService } from './services/api.service';
 import { ErrorService } from './services/error.service';
@@ -12,7 +11,6 @@ import { ErrorService } from './services/error.service';
 export class AppComponent implements OnInit {
  
   constructor(
-   public errorService: ErrorService,
    public api:ApiService
   ) {
     
@@ -22,20 +20,8 @@ export class AppComponent implements OnInit {
     
     this.api.request('user', 'get', undefined, '20').subscribe((x) => {
       console.log(x);
-    })
+    });
 
-    this.errorService.onError.subscribe((message) => {      
-       SWAL.fire({
-          position: 'top-end',
-          imageUrl: 'https://thumbs.dreamstime.com/b/stressed-virtual-reality-developer-out-programmer-working-game-app-79348096.jpg',
-          imageWidth: 400,
-          imageHeight: 266,
-          title: 'Gentleman, we have a problem with the http request',
-          timer: 5000,
-          backdrop: false,
-          showConfirmButton:false
-          })
-    }) ;
     // this.errorService.onError.next('Test error');
   } 
   
