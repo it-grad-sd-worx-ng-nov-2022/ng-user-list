a8e2203673a0f3c24644c50a721a50913a8fefdf
youshra <youshrayoush@gmail.com>
2022-12-12 00:08:14 +0400

    homepage_cards_redirection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { UserListComponent } from './components/user-list/user-list.component';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { HomeComponent } from './components/home/home.component';
 
+
 const routes: Routes = [
   {path:'user', component:UserListComponent, children:[
     {path:'view/:id', component:UserDetailsComponent}]},
-  {path:'', component:HomeComponent}
+
+  {path:'', component:HomeComponent},
+  {path:'user/:seed', component:UserListComponent}
+  
 ];
 
 @NgModule({
--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { ApiService } from 'src/app/services/api.service';
 
@@ -11,21 +12,35 @@ export class UserListComponent implements OnInit {
 
   filteredUserData?:User[];
   //filteredUserData?:User[] = [];
-  apiRequestSeed:string = 'Students';// default seed is Students
+  apiRequestSeed: any;
   userData:User[] = [];
 
-  constructor(public api:ApiService) { }
+  constructor(public api:ApiService,
+    public activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+  
+    this.apiRequestSeed = this.activatedRoute.snapshot.paramMap.get('seed');//initially retrieve from param
+    console.log('after paramMap ', this.apiRequestSeed); 
+
+    if(this.apiRequestSeed){
+      this.getSeedUsers();
+      console.log("inside if",this.apiRequestSeed);
+    }else{
+      this.apiRequestSeed ='Students';
+      this.getSeedUsers();
+      console.log("inside else ",this.apiRequestSeed);
+    }
+
     this.filteredUserData = this.userData;
-    //calling the users on page load with initial value of students
-    this.getSeedUsers();
   }
+
 
   setData(filteredData:User[]) {
     console.log("waza",filteredData);
     this.filteredUserData = filteredData;
   }
+
 
   getSeedUsers() {
     console.log("Call API with the following seed ->", this.apiRequestSeed);
