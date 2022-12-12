f52d783da4e03e73bda88400ca5cd0b41a5741b4
Jessen26 <jessenmungapen4@gmail.com>
2022-12-12 10:18:51 +0400

    Footer Theme Applied

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingService } from '../../../services/setting.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+currentTheme:string ='light';
+  constructor(
+    public setting:SettingService
+  ) { }
 
   ngOnInit(): void {
+    this.setting.outsetTheme.subscribe(newTheme => {
+      console.log("newTheme", newTheme);
+      this.currentTheme = newTheme;
+    });
   }
 
 }
